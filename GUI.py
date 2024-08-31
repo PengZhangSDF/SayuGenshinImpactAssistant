300bc9340f914521ed0af7e7d6dd60ef5f7e42d3
PengZhangSDF <1307717743@qq.com>
2024-08-31 13:50:52 +0800

    GUI修正

--- GUI.py
+++ GUI.py
@@ -22,7 +22,7 @@ config = configparser.ConfigParser()
 config.read(config_file_path)
 
 window = Tk()
-window.title(f"原神自动化程序 V1.0.1                           Github主页：https://github.com/PengZhangSDF/GIAA   欢迎提交issue           ")
+window.title(f"早柚原神自动化 V0.0.1                           Github主页：https://github.com/PengZhangSDF/GIAA   欢迎提交issue           ")
 window.geometry("1032x795")
 window.configure(bg="#FFFFFF")
 button_images = {}
@@ -724,7 +724,7 @@ canvas.create_text(
     79.0,
     20.0,
     anchor="nw",
-    text="GI:原神自动化   GIAA V1.0.1  GUI V1.0.1                                                  我的GUI学的MAA",
+    text="SGIA:早柚原神自动化   V0.0.1  GUI V0.0.1                                                  我的GUI学的MAA",
     fill="#000000",
     font=("Inter", 16 * -1)
 )
